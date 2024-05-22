19b3f12d1c169655882b967b929daef845c7b101
Blaine Wang <blaine.wang@outlook.com>
2024-05-22 21:40:46 +0800

    init project

---
+++ src/main/java/com/simplerssview/RssViewerApplication.java
@@ -0,0 +1,11 @@
+package com.simplerssview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssViewerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RssViewerApplication.class, args);
+    }
+}
